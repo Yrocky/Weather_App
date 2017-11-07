b7ca184850ab5387a4c3690b9eb08b21e78eeb23
yrocky <983272765@qq.com>
2017-11-07 14:00:01 +0800

    ad some

--- Weather_App/MMNavigationController.m
+++ Weather_App/MMNavigationController.m
@@ -7,32 +7,124 @@
 //
 
 #import "MMNavigationController.h"
+#define kNavigationBarBackImageName @"img_nav_bg_7.png"
 
-@interface MMNavigationController ()
+@interface MMNavigationController ()<UINavigationControllerDelegate,UIGestureRecognizerDelegate>
 
 @end
 
 @implementation MMNavigationController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
 }
 
-- (void)didReceiveMemoryWarning {
+// fix: 导航栏的返回按钮位置不正确，使用自定义的button进行设置返回按钮又导致侧滑手势失灵。http://www.jianshu.com/p/349636eb3fca
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self setupCustomNavigation];
+    __weak MMNavigationController *weakSelf = self;
+    
+    if ([self respondsToSelector:@selector(interactivePopGestureRecognizer)])
+    {
+        self.interactivePopGestureRecognizer.delegate = weakSelf;
+        self.delegate = weakSelf;
+    }
+}
+
+
+#pragma mark UINavigationControllerDelegate
+
+- (void)navigationController:(UINavigationController *)navigationController
+       didShowViewController:(UIViewController *)viewController
+                    animated:(BOOL)animate
+{
+    // Enable the gesture again once the new controller is shown
+    
+    if ([self respondsToSelector:@selector(interactivePopGestureRecognizer)])
+        self.interactivePopGestureRecognizer.enabled = YES;
+}
+
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
+    
+    if ([self respondsToSelector:@selector(interactivePopGestureRecognizer)]){
+        self.interactivePopGestureRecognizer.enabled = NO;
+    }
+    if (self.childViewControllers.count >= 1) {
+        // mm_common_navi_bar_back@2x
+        // 左上角的返回按钮
+        UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        backButton.frame = (CGRect){CGPointZero,CGSizeMake(60, 30)};
+        [backButton setImage:[UIImage imageNamed:@"back"] forState:UIControlStateNormal];
+        [backButton setImage:[UIImage imageNamed:@"back"] forState:UIControlStateHighlighted];
+        [backButton setTitle:@"" forState:UIControlStateNormal];
+        [backButton setTitle:@"" forState:UIControlStateHighlighted];
+        backButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+        [backButton addTarget:self action:@selector(back:) forControlEvents:UIControlEventTouchUpInside];
+        backButton.contentEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 0);
+//        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+//        viewController.hidesBottomBarWhenPushed = YES; // 隐藏底部的工具条
+    }
+    [super pushViewController:viewController animated:animated];
+}
+
+- (void) back:(UIButton *)button{
+    [self popViewControllerAnimated:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//运行试一试，两个问题同时解决，不过又发现了新问题，手指在滑动的时候，被 pop 的 ViewController 中的 UIScrollView 会跟着一起滚动，这个效果看起来就很怪（知乎日报现在就是这样的效果），而且也不是原始的滑动返回应有的效果，那么就让我们继续用代码来解决吧
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldBeRequiredToFailByGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    return [gestureRecognizer isKindOfClass:UIScreenEdgePanGestureRecognizer.class];
 }
-*/
+
+#pragma mark UINavigationControllerDelegate
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    if ([self.childViewControllers count] == 1) {
+        return NO;
+    }
+    return YES;
+}
+
+// 我们差不多能猜到是因为手势冲突导致的，那我们就先让 ViewController 同时接受多个手势吧。
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    return YES;
+}
+
+#pragma mark - setup part.
+
+- (void)setupCustomNavigation
+{
+    self.navigationBar.translucent = NO;//设置navigationbar的半透明
+}
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    //    if(self.isSupportLandScape){
+    //        return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskLandscapeLeft;
+    //    }
+    //    else{
+    return UIInterfaceOrientationMaskPortrait;
+    // }
+}
 
 @end
