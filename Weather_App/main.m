235819eadf98fbb13c927c9898731c190bf28907
yrocky <983272765@qq.com>
2018-09-12 10:30:03 +0800

    b commit

--- Weather_App/main.m
+++ Weather_App/main.m
@@ -67,7 +67,7 @@ int main(int argc, char * argv[]) {
         //
         NSLog(@"compera:%d",[other compare:date]);
         
-        
+        NSLog(@"...b...");
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
