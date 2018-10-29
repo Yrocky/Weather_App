22c165d351e52db016e8dd78f7f4c5c7c1e1cf7d
Yrocky <983272765@qq.com>
2018-10-30 00:05:38 +0800

    add:自定义gesture

--- Weather_App/GiftEffectViewController.m
+++ Weather_App/GiftEffectViewController.m
@@ -9,16 +9,73 @@
 #import "GiftEffectViewController.h"
 #import "Masonry.h"
 #import "GIftEffectView/GiftEffect/GiftShapeEffectView.h"
+#import "MMDirectionGestureRecognizer.h"
 
 @interface GiftEffectViewController ()
 @property (nonatomic ,strong) GiftShapeEffectView * effectView;
+
+@property (nonatomic ,strong) UILabel * directionLabel;
+@property (nonatomic ,strong) UIView * circleView;
 @end
 
 @implementation GiftEffectViewController
 
+
+- (void) onDirectoinGesture:(MMDirectionGestureRecognizer *)gesture{
+    
+    if (gesture.state == UIGestureRecognizerStateBegan) {
+        self.circleView.hidden = NO;
+        self.circleView.center = [gesture locationInView:self.view];
+    }
+    else if (gesture.state == UIGestureRecognizerStateChanged) {
+        
+        NSString * direction = @"";
+        if (gesture.direction == MMDirectionGestureRecognizerUp) {
+            direction = @"up";
+        }
+        if (gesture.direction == MMDirectionGestureRecognizerDown) {
+            direction = @"down";
+        }
+        if (gesture.direction == MMDirectionGestureRecognizerLeft) {
+            direction = @"left";
+        }
+        if (gesture.direction == MMDirectionGestureRecognizerRight) {
+            direction = @"right";
+        }
+        self.directionLabel.text = direction;
+    }else{
+        self.circleView.hidden = YES;
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.directionLabel = [UILabel new];
+    self.directionLabel.textAlignment = NSTextAlignmentCenter;
+    self.directionLabel.textColor = [UIColor colorWithRed:0.91 green:0.26 blue:0.34 alpha:1.00];
+    [self.view addSubview:self.directionLabel];
+    [self.directionLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.mas_equalTo(self.view);
+        make.top.mas_equalTo(self.view);
+    }];
+    
+    MMDirectionGestureRecognizer * gesture = [[MMDirectionGestureRecognizer alloc] initWithTarget:self action:@selector(onDirectoinGesture:)];
+    gesture.hysteresisOffset = 40;
+    [self.view addGestureRecognizer:gesture];
+    
+    self.circleView = [UIView new];
+    self.circleView.layer.borderColor = [UIColor redColor].CGColor;
+    self.circleView.layer.borderWidth = 1;
+    self.circleView.bounds = CGRectMake(0, 0, gesture.hysteresisOffset * 2, gesture.hysteresisOffset * 2);
+    self.circleView.layer.cornerRadius = gesture.hysteresisOffset;
+    self.circleView.layer.masksToBounds = YES;
+    self.circleView.hidden = YES;
+    [self.view addSubview:self.circleView];
+    
+    return;
     self.effectView = [[GiftShapeEffectView alloc] init];
     self.effectView.backgroundColor = [UIColor greenColor];
     [self.view addSubview:self.effectView];
@@ -50,6 +107,7 @@
         }
     }];
 }
+
 
 - (void)sendGift {
     
