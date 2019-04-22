1de8cb5a6424da7d9c463b2f6a5d5b1c0f340d42
Yrocky <983272765@qq.com>
2019-04-22 23:58:52 +0800

    add:添加一个用于测试的分享插件以及本地html文件

---
+++ Weather_App/MMSharePlugin.h
@@ -0,0 +1,17 @@
+//
+//  MMSharePlugin.h
+//  Weather_App
+//
+//  Created by Rocky Young on 2019/4/22.
+//  Copyright © 2019 Yrocky. All rights reserved.
+//
+
+#import "MMPlugin.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MMSharePlugin : MMPlugin
+
+@end
+
+NS_ASSUME_NONNULL_END
